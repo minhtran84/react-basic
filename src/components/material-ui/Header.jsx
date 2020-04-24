5d484763bcd92ae44bec8ea974f789c644a2d64e
Minh Tran <minhtran.se@gmail.com>
2020-04-24 22:25:43 +0700

    a commit

--- src/components/material-ui/Header.jsx
+++ src/components/material-ui/Header.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import AppleIcon from '@material-ui/icons/Apple';
-import { makeStyles } from '@material-ui/styles';
-
-const useStyles = makeStyles(() => ({
-   headerTypography: {
-       flex: 1
-   } 
-}))
+import styles from './Header.module.css';
 
 const Header = () => {
-    const classes = useStyles();
     return (
         <AppBar position="static">
             <Toolbar>
-                <Typography className={classes.headerTypography}>Your Company</Typography>
+                <Typography className={styles.header}>Your Company</Typography>
                 <AppleIcon />
             </Toolbar>
         </AppBar>
